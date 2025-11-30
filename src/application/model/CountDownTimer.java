1a759abffc67578fad3075da39c406bedcff8895
Owl <mrmoor031305@gmail.com>
2025-11-30 01:07:21 -0600

    forgot countdowntimer and mole, i'm stupid

--- src/application/model/CountDownTimer.java
+++ src/application/model/CountDownTimer.java
@@ -1,5 +1,16 @@
 package application.model;
 
-public class CountDownTimer {
+import application.view.MainView;
+
+public class CountDownTimer implements Runnable {
+	WhackAMole game;
+	MainView mainView;
+	int DurationsSeconds;
+
+	@Override
+	public synchronized void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/application/model/Mole.java
+++ src/application/model/Mole.java
@@ -1,5 +1,19 @@
 package application.model;
 
-public class Mole {
+import java.util.*;
+import application.view.MainView;
+import javafx.scene.image.*;
 
+public class Mole implements Runnable {
+	WhackAMole game;
+	MainView mainView;
+	Image moleImage;
+	int index;
+	Random rand;
+
+	@Override
+	public synchronized void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
