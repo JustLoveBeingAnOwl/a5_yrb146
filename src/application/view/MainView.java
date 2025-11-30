ae15eafd2a16b28592ccfd97a957cd3e1e9acb3b
Owl <mrmoor031305@gmail.com>
2025-11-30 00:36:54 -0600

    create the controller, model, view packages + do work on view

---
+++ src/application/view/MainView.java
@@ -0,0 +1,22 @@
+package application.view;
+
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+
+public class MainView {
+	Label timeRemainingLabel;
+	Label scoreLabel;
+	ImageView[] imageViews;
+	
+	public void displayTimeRemaining(String time) {
+		
+	}
+	
+	public void displayLabel(String score) {
+		scoreLabel.setText(score);
+	}
+	
+	public void displayImage(int index, Image image) {
+		
+	}
+}
