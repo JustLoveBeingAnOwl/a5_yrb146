5388f7c605e0c6d6b1e8cf014da87fe86918475d
Andrew Morin <andrewaddaimorin@gmail.com>
2025-12-02 12:30:24 -0600

    Refactor initialize method to streamline setup

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -24,13 +24,31 @@ public class MainController {
 	@FXML private Pane pane4;
 	
 	public void initialize() {
+		Pane[] panes = {pane0, pane1, pane2, pane3, pane4};
+		for(Pane p : panes) {
+			p.setStyle("-fx-border-color: black; -fx-border-width: 2;");
+		}
 		
+		ImageView[] views = {imageView0, imageView1, imageView2, imageView3, imageView4};
+		int i = 0;
+		for(ImageView v : views) {
+			v.setUserData(i);
+			i++;
+		}
+		
+		Image moleImage = new Image(getClass().getResourceAsStream("/application/mole-1.png"));
+		Image blank = null;
+		
+		MainView view = new MainView(timeRemainingLabel, scoreLabel, views);
+		game = new WhackAMole(view, moleImage, blank);
 	}
 	public void startButtonAction(){
-		
+		game.startGame();
 	}
 
 	public void imageViewAction(){
-		
+		ImageView image = (ImageView)e.getSource();
+		int ind = (int) image.getUserData();
+		game.whackMole(ind);
 	}
 }
