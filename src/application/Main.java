2f1db9d0f71c62b83bb6aa01737f23ac0fcf1502
Andrew Morin <andrewaddaimorin@gmail.com>
2025-12-02 12:31:32 -0600

    Load FXML and set up scene in Main class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,21 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.Parent;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
-
+			Parent root = FXMLLoader.load(getClass().getResource("WhackAMole.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Whack-A-Mole");
+			primaryStage.setScene(scene);
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
