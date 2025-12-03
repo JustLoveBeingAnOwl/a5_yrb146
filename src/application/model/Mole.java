78f364f56c2b9b450af982d0e9e4fb9933491c2e
Diegox4521 <129859254+Diegox4521@users.noreply.github.com>
2025-12-02 22:14:10 -0600

    Refactor Mole class and implement game logic
    Refactor Mole class by consolidating fields and updating the run method to include game logic.

--- src/application/model/Mole.java
+++ src/application/model/Mole.java
@@ -1,43 +1,55 @@
 package application.model;
 
-import java.util.*;
+import java.util.Random;
+
 import application.view.MainView;
-import javafx.scene.image.*;
+import javafx.scene.image.Image;
+
 
 public class Mole implements Runnable {
-	WhackAMole game;
-	MainView mainView;
-	Image moleImage;
-	int index;
-	Random rand;
+    private WhackAMole game;
+    private MainView mainView;
+    private Image moleImage;
+    private int index;
+    private Random rand = new Random();
 
-	public Mole(WhackAMole game, MainView mainView, Image moleImage, int index) {
-		this.game = game;
-		this.mainView = mainView;
-		this.moleImage = moleImage;
-		this.index = index;
-	}
-	
-	public void setGame(WhackAMole game) {
-		this.game = game;
-	}
-	public void setMainView(MainView mainView) {
-		this.mainView = mainView;
-	}
-	public void setMoleImage(Image moleImage) {
-		this.moleImage = moleImage;
-	}
-	public void setIndex(int index) {
-		this.index = index;
-	}
-	
-	public int getIndex() {
-		return index;
-	}
-	
-	@Override
-	public synchronized void run() {
-		// TODO Auto-generated method stub
-		
-	}
+    public Mole(WhackAMole game, MainView mainView, Image moleImage, int index) {
+        this.game = game;
+        this.mainView = mainView;
+        this.moleImage = moleImage;
+        this.index = index;
+    }
+
+    @Override
+    public void run() {
+        while (!game.gameOver()) {
+            try {
+                game.setExposed(index, false);
+                mainView.displayImage(index, null);
+
+                Thread.sleep(2000 + rand.nextInt(3001));
+
+                game.exposureStart[index] = System.currentTimeMillis();
+                game.setExposed(index, true);
+                mainView.displayImage(index, moleImage);
+
+                Thread.sleep(1000 + rand.nextInt(1001));
+
+                game.setExposed(index, false);
+                mainView.displayImage(index, null);
+            } catch (InterruptedException e) {
+                if (game.gameOver()) {
+                    return;
+                }
+
+                long elapsed = System.currentTimeMillis() - game.exposureStart[index];
+                if (elapsed < 0) elapsed = 0;
+                game.updateScore((int) elapsed);
+
+                game.setExposed(index, false);
+                mainView.displayImage(index, null);
+
+            }
+        }
+    }
 }
