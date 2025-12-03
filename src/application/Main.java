ab6567a0e53a4c7fcd8aa809d6f226ff62d2845a
Diegox4521 <129859254+Diegox4521@users.noreply.github.com>
2025-12-02 22:13:21 -0600

    Load FXML in start method for Whack-A-Mole game
    Updated the start method to load FXML for Whack-A-Mole.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,26 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("WhackAMole.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Whack-A-Mole");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
