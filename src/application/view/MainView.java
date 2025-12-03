2d10cfc35b5c0f1b485a5239405efb8da7ff508c
Diegox4521 <129859254+Diegox4521@users.noreply.github.com>
2025-12-02 22:13:56 -0600

    Refactor MainView for thread-safe UI updates
    Refactor MainView methods to use Platform.runLater for UI updates and improve null checks.

--- src/application/view/MainView.java
+++ src/application/view/MainView.java
@@ -1,47 +1,75 @@
 package application.view;
 
-import javafx.scene.control.*;
-import javafx.scene.image.*;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainView {
-	private Label timeRemainingLabel;
-	private Label scoreLabel;
-	private ImageView[] imageViews;
-	private Image moleImage;
-	
-	public void setTimeRemainingLabel(Label timeRemainingLabel) {
-		this.timeRemainingLabel = timeRemainingLabel;
-	}
-	
-	public void setScoreLabel(Label scoreLabel) {
-		this.scoreLabel = scoreLabel;
-	}
-	
-	public void setImageViews(ImageView... imageViews ) {
-		this.imageViews = imageViews;
-	}
-	
-	public void displayTimeRemaining(String time) {
-		timeRemainingLabel.setText("Time Remaining:	"+time);
-	}
-	
-	public void displayScoreLabel(String score) {
-		scoreLabel.setText("Score:	"+score);
-	}
-	
-	public void displayImage(int index, Image image) {
-		imageViews[index].setImage(image);
-	}
-	
-	public ImageView getImageView(int index) {
-		return imageViews[index];
-	}
-	
-	public Image getMoleImage() {
-		return moleImage;
-	}
 
-	public void setMoleImage(Image image) {
-		this.moleImage = image;
-	}
+    private Label timeRemainingLabel;
+    private Label scoreLabel;
+    private ImageView[] imageViews;
+    private Image moleImage;
+
+    public void setTimeRemainingLabel(Label label) {
+        this.timeRemainingLabel = label;
+    }
+
+    public void setScoreLabel(Label label) {
+        this.scoreLabel = label;
+    }
+
+    public void setImageViews(ImageView... views) {
+        this.imageViews = views;
+    }
+
+    public void setMoleImage(Image img) {
+        this.moleImage = img;
+    }
+
+    public Image getMoleImage() {
+        return moleImage;
+    }
+
+    public void displayTimeRemaining(String time) {
+        Platform.runLater(() -> {
+            if (timeRemainingLabel != null) {
+                timeRemainingLabel.setText(time);
+            }
+        });
+    }
+
+    public void displayScore(String score) {
+        Platform.runLater(() -> {
+            if (scoreLabel != null) {
+                scoreLabel.setText(score);
+            }
+        });
+    }
+
+    public void displayImage(int index, Image img) {
+        Platform.runLater(() -> {
+            if (imageViews != null && index >= 0 && index < imageViews.length) {
+                imageViews[index].setImage(img);
+            }
+        });
+    }
+
+    public ImageView getImageView(int index) {
+        if (imageViews != null && index >= 0 && index < imageViews.length) {
+            return imageViews[index];
+        }
+        return null;
+    }
+
+    public void hideAllMoles() {
+        Platform.runLater(() -> {
+            if (imageViews != null) {
+                for (ImageView iv : imageViews) {
+                    if (iv != null) iv.setImage(null);
+                }
+            }
+        });
+    }
 }
